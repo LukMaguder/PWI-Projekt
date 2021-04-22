91b363ee459cafe6354aac58690a3e60aaf6c1b7
Lukasz Maguder <47333933+LukMaguder@users.noreply.github.com>
2021-04-22 20:43:46 +0200

    Delete testowy.php

--- testowy.php
+++
@@ -1,9 +0,0 @@
-for($i=1;$i<=10;$i++) 
-    	{
-    		$wypadlo = rand(1,6);
-    		echo("Rzut nr $i:<br>n");
-    		echo("<b>$wypadlo</b><br>n");
-  if($wypadlo==6)
-      echo("wygrana!")
-    }
-  
